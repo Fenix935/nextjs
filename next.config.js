95f82e15bf9f40b0e8f552ce7513a58bbc878e37
Fenix935 <59768933+Fenix935@users.noreply.github.com>
2024-01-10 22:01:21 +0000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     env: {
-        "BASE_URL": "https://nextjs-6twlnln65-fenix935s-projects.vercel.app",
+        "BASE_URL": "https://nextjs-git-new-fenix935s-projects.vercel.app",
         "MONGODB_URL": "mongodb+srv://fenixfire5454:Ihy4womyUCEEtHnM@cluster0.fq31og8.mongodb.net/tuit?retryWrites=true&w=majority",
         "ACCESS_TOKEN_SECRET": "YOUR_ACCESS_TOKEN_SECRET",
         "REFRESH_TOKEN_SECRET": "YOUR_REFRESH_TOKEN_SECRET",
